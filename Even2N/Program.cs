ecbc2de8b03a2af5a36786ad7cf21d3e5c0be634
Oleg-GH <fan2004g@bk.ru>
2022-10-14 21:39:30 +0500

    Задача напечатать все четные числа от 1 до N

---
+++ Even2N/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача выдачи всех четных чисел от 1 до N по введенному N 
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int result = 2;
+
+Console.Write($"{number} -> ");
+
+while(result < (number + 1))
+{
+    if(result < (number - 1))
+    {
+        Console.Write($"{result}, ");
+        result = result + 2;
+    }
+    else 
+    {
+        Console.WriteLine($"{result} ");
+        result = result + 2;
+    }
+}
