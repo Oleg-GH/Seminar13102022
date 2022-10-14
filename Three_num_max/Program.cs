56ec328f4960ddec91936e8cdb1982373f37f8bc
Oleg-GH <fan2004g@bk.ru>
2022-10-14 20:03:15 +0500

    Задача о поиске мах из трех чисел

---
+++ Three_num_max/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача о выдаче максимального числа из трёх
+Console.Clear();
+Console.WriteLine("Введите три числа и получите максимальное: ");
+int numberA = int.Parse(Console.ReadLine()); 
+int numberB = int.Parse(Console.ReadLine()); 
+int numberC = int.Parse(Console.ReadLine()); 
+
+int max = numberA;
+
+if(numberB > max)
+{
+    max = numberB;
+}
+else
+{
+    if(numberC > max)
+
+    {
+        max = numberC;
+    }
+}
+
+Console.WriteLine($"{numberA}, {numberB}, {numberC} -> {max}");
