841f6c19e91740e3b67c1fb3077bdb41710bab14
Oleg-GH <fan2004g@bk.ru>
2022-10-14 20:49:24 +0500

    Задача определения четности числа

---
+++ Even2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача определения четности числа 
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if(number%2 == 0)
+{
+    Console.WriteLine($"{number} -> Да");
+}
+else
+{
+    Console.WriteLine($"{number} -> Нет");
+}
