cdca8562d2948f1365bf5db7b6e81c677b8aa1d1
Oleg-GH <fan2004g@bk.ru>
2022-10-14 18:09:28 +0500

    Задача нахождения максимального из двух чмсел

---
+++ Two_num_max/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача о выдаче максимального числа из двух
+Console.Clear();
+Console.Write("Введите первое число: "); 
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+
+if(numberA > numberB)
+{
+    Console.WriteLine($"max = {numberA}");
+}
+
+else
+{
+    Console.WriteLine($"max = {numberB}");
+}
