e9cc9d6c86a1a16f01804b8a145c0ca6f23918ca
Oleg-GH <fan2004g@bk.ru>
2022-10-14 20:23:20 +0500

    Исправил вывод данных для задачи по поиску максимального числа из двух

--- Two_num_max/Program.cs
+++ Two_num_max/Program.cs
@@ -1,16 +1,15 @@
 ﻿//Задача о выдаче максимального числа из двух
 Console.Clear();
-Console.Write("a = "); 
-int numberA = int.Parse(Console.ReadLine());
-Console.Write("b = ");
+Console.WriteLine("Введите два числа: "); 
+int numberA = int.Parse(Console.ReadLine()); 
 int numberB = int.Parse(Console.ReadLine());
 
 if(numberA > numberB)
 {
-    Console.WriteLine($"max = {numberA}");
+    Console.WriteLine($"a = {numberA}, b = {numberB}, -> max = {numberA}");
 }
 
 else
 {
-    Console.WriteLine($"max = {numberB}");
+    Console.WriteLine($"a = {numberA}, b = {numberB}, -> max = {numberB}");
 }
